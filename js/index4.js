a36d751f773dcf8d2abdd7d4f7645894406abc10
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2023-11-25 19:55:23 +0200

    HW4

---
+++ js/index4.js
@@ -0,0 +1,24 @@
+// function checkAge(age) {
+//   return age > 18 ? true : confirm("Батьки дозволили?");
+// }
+// checkAge(10);
+
+// function min(a, b) {
+//   return a < b ? a : b;
+// }
+// min(7, 12);
+
+const ask = (question, yes, no) => {
+  if (confirm(question)) yes();
+  else no();
+};
+
+ask(
+  "Ви згодні?",
+  () => {
+    alert("Ви погодились.");
+  },
+  () => {
+    alert("Ви скасували виконання.");
+  }
+);
